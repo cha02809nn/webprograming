1753fe5545cfc18dcf69697dff33902ce17e1f3c
Sirchai Daengchaona <cha02809nn@gmail.com>
2021-07-12 03:44:56 +0700

    spread + Destruturing

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
-let x = 10;
+// Destructuring
 
-function show() {
-    x = 20;
-    return x;
+let obj = {
+    name: "Sirchai",
+    lastname: "Daengchaona",
+    age: 20,
+    salary: 10000
 }
 
-console.log(x);
-console.log(show());
-console.log(x);
+let obj2 = {
+    name: "Museum",
+    lastname: "Dindee",
+    age: 25,
+    salary: 18000
+}
+
+const showdata = ({name, lastname, age, salary}) =>`สวัสดีคุณ ${name} ${lastname}
+ขณะนี้คุณมีอายุ ${age}
+เงินเดือนของคุณ ${salary}`;
+
+console.log(showdata(obj));
+console.log(showdata(obj2));
+
+// Spread Operator
+
+const fun = (x, y, z) => x + y + z;
+
+let num = [5, 6, 7];
+
+let res = fun(...num); //...num  fun(5, 6, 7);
+
+console.log(res);
